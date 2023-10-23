cf46247e857e1c2ea632737cc835b864ff1472c3
joaogbr27 <127787793+joaogbr27@users.noreply.github.com>
2023-10-23 17:31:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert('Hello World!');
+function tocarTeclaGrilo(){
+    document.querySelector('.som_tecla_grilo').onclick = tocarTeclaGrilo;
+}
+function teclaaplausos(){
+    document.querySelector('.som_tecla_aplausos').onclick = teclaaplausos;
+}
+function teclavaia(){
+    document.querySelector('.som_tecla_vaia').onclick = teclavaia;
+}
+function teclarisada(){
+    document.querySelector('som_tecla_risada').onclick = teclarisada;
+}
+function teclaerrado(){
+    document.querySelector('som_tecla_errado').onclick = teclaerrado;
+}
+function teclatambores(){
+    document.querySelector('som_tecla_tambores').onclick = teclatambores;
+}
+function teclatrombeta(){
+    document.querySelector('som_tecla_trombeta').onclick = teclatrombeta;
+}
+function tecladinheiro(){
+    document.querySelector('som_tecla_dinheiro').onclick = tecladinheiro;
+}
+function teclavitoria(){
+    document.querySelector('som_tecla_vitoria').onclick = teclavitoria;
+}
