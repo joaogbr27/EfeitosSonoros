68fb62cf8324c0888fdf11465f8ee1f914774552
joaogbr27 <127787793+joaogbr27@users.noreply.github.com>
2023-11-13 17:10:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-function tocarTeclaGrilo(){
-    document.querySelector('.som_tecla_grilo').onclick = tocarTeclaGrilo;
+function tocaSomAplausos(){
+    document.querySelector("#som_tecla_aplausos").play();   
 }
-function teclaaplausos(){
-    document.querySelector('.som_tecla_aplausos').onclick = teclaaplausos;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[2].onclick = tocaSomAplausos
+
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador<listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSomAplausos;
+    contador = contador + 1;
 }
-function teclavaia(){
-    document.querySelector('.som_tecla_vaia').onclick = teclavaia;
-}
-function teclarisada(){
-    document.querySelector('som_tecla_risada').onclick = teclarisada;
-}
-function teclaerrado(){
-    document.querySelector('som_tecla_errado').onclick = teclaerrado;
-}
-function teclatambores(){
-    document.querySelector('som_tecla_tambores').onclick = teclatambores;
-}
-function teclatrombeta(){
-    document.querySelector('som_tecla_trombeta').onclick = teclatrombeta;
-}
-function tecladinheiro(){
-    document.querySelector('som_tecla_dinheiro').onclick = tecladinheiro;
-}
-function teclavitoria(){
-    document.querySelector('som_tecla_vitoria').onclick = teclavitoria;
-}
+
